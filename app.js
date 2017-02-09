9f36842300e42c0932ccf63a1bb13e377f182cfd
Victor Macias <v.maciascastillo@gmail.com>
2017-02-09 18:54:30 +0100

    Print api return

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const https = require('https');
-const username = "victormaciascastillo";
+const username = "chalkers";
 
 // function to print message to console
 function printMessage(username, badgeCount, point) {
@@ -7,9 +7,25 @@ function printMessage(username, badgeCount, point) {
 	console.log(message);
 }
 
-printMessage("vic0707", 100, 20000);
+// printMessage("vic0707", 100, 20000);
 
-// Connect to the API URL
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-	console.dir(response);
-});
+function getProfile(username) {
+	// Connect to the API URL
+	const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+		// console.log(response.statusCode);
+		let body = "";
+		response.on('data', data => {
+			body += data.toString();
+		});
+		response.on('end', () => {
+			const profile = JSON.parse(body);
+			printMessage(username, profile.badges.length, profile.points.JavaScript);
+			// console.dir(profile);
+			// console.log(body);
+		});
+	});
+}
+
+const users = ["chalkers", "alenaholligan"];
+users.forEach(getProfile);
+// getProfile("alenaholligan");
